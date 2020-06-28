468f1e7fc574b395736ce6d20f1384310fea425e
Mary Ma <maxiuli95@gmail.com>
2020-06-28 16:27:52 +0800

    polish preview styke

--- src/components/Preview/index.js
+++ src/components/Preview/index.js
@@ -7,7 +7,15 @@ export default ({ list }) => {
       {list.map(gif => {
         return (
           <div className="item" key={gif.name}>
-            <img src={gif.src} alt={gif.name} />
+            {gif.width > 400 && (
+              <img
+                width={gif.width / 2}
+                height={gif.height / 2}
+                src={gif.src}
+                alt={gif.name}
+              />
+            )}
+            {gif.width <= 400 && <img src={gif.src} alt={gif.name} />}
             <div className="item-info">
               <div>Width: {gif.width}</div>
               <div>Height: {gif.height}</div>
